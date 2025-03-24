273e2213044b69dbf3a798e6660d27f57d0a87b4
Jesse Osrecak <jesse.osrecak@gmail.com>
2025-03-24 17:58:45 +1100

    Added Plans template

---
+++ coaching-by-celeste/src/app/components/plan/plan.js
@@ -0,0 +1,23 @@
+// import "./plan.css";
+import styles from "./plan.css";
+
+export default function Plan(){
+    return(
+    <div className="plans-container">
+        <div className="plans_inner_container">
+            <h3>
+                Plan title
+            </h3>
+            <ul>
+                <li>
+                    Item a
+                </li>
+                <li>
+                    Item b
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    );
+}
--- coaching-by-celeste/src/app/page.js
+++ coaching-by-celeste/src/app/page.js
@@ -1,11 +1,21 @@
 import styles from "./page.module.css";
+import Plan from "./components/plan/plan";
+// import IMG from "https://picsum.photos/id/1/200/300"
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Home
+        <h1>
+          Coaching By Celeste
+        </h1>
+        <img className={styles.hero_img} src="https://picsum.photos/id/65/200/300"/>
+        <Plan/>
+
       </main>
+        
+
       <footer className={styles.footer}>
         
       </footer>
