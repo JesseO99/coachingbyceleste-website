4c6299fea337c1ae63367df94f45b4aa89993c5e
Jesse Osrecak <jesse.osrecak@gmail.com>
2026-02-07 15:37:14 +1100

    added some content more work needed to complete but switching workstations

--- coaching-by-celeste/src/app/eight_week_challenge/page.jsx
+++ coaching-by-celeste/src/app/eight_week_challenge/page.jsx
@@ -7,8 +7,22 @@ export default function Page() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className={styles.heading}>The Women's 8 Week Challenge</h1>
+        <p className={styles.subheading}>COACHING BY CELESTE</p>
         <div className={styles.competition}>
-          <div className={styles.competition_details}><h2>What's Included?</h2>
+          <div className={styles.introduction}>
+            <p>
+              Are you struggling to lose weight and stay consistent? <br/> <br/> 
+
+              This 8 week challenge is for you! <br/> <br/> 
+
+              Over the 8 weeks you will get a clear plan to lose weight that is designed specifically for women! <br/> <br/>
+
+              <strong className={styles.start_date}>Start date 2nd of March</strong>
+            </p>
+          </div>
+          <div className={styles.competition_img}> <img src="eight_week_challenge.jpg" /></div>
+        </div>
+        <div className={styles.competition_details}><h2>What's Included?</h2>
           <ul>
             <li className={styles.inclusions}>2 Personal Training Sessions</li>
             <li className={styles.inclusions}>2 EVOLT Body Comp Scans</li>
@@ -19,8 +33,6 @@ export default function Page() {
           </ul>
           <p><strong> Upgrade me option: </strong> As part of the challenge i'm offering discounted additional personal training sessions priced at $65 for 45min (save $15 per session)</p>
           </div>
-          <div className={styles.competition_img}> <img src="eight_week_challenge.jpg" /></div>
-        </div>
         <div className={styles.competition_winner}>
           <h2>$600 worth of Prizes</h2>
           <ul>
@@ -44,14 +56,14 @@ export default function Page() {
             link={"https://share.newie.app/offerings/1B825DBD-4A11-4E73-B85E-54C09466FF33"} 
           />
           <Package
-            payment = {""}
-            frequency = {"Have a question?"}
+            payment = {"Have a question?"} //TODO Change to a unique block and make font smaller
+            frequency = {""}
             button_text = {"Find Out More"} 
             link={"https://form.jotform.com/260319100518851"} 
           />
 
         </div>
-        <Contact_form/>
+        Instagram logo @coaching by celeste
       </main>
     </div>
   );
